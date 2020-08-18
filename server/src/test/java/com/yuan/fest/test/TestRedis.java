f0b2518626cdde574270eb3e1224e1e3cdb07806
Mr.wang <52198480+Superwang6@users.noreply.github.com>
2020-08-19 00:09:06 +0800

    解决redis集成问题

--- server/src/test/java/com/yuan/fest/test/TestRedis.java
+++ server/src/test/java/com/yuan/fest/test/TestRedis.java
@@ -5,26 +5,41 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("pro")
 public class TestRedis {
 
     @Autowired
     private RedisTemplate redisTemplate;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     public void testRedis(){
         ValueOperations valueOperations = redisTemplate.opsForValue();
-        valueOperations.set("laowang","lao");
+        valueOperations.set("hahah","yingyingying");
+
+//        System.out.println(valueOperations.get("laowang"));
+    }
+
+    @Test
+    public void testRedis2(){
+        ValueOperations valueOperations = stringRedisTemplate.opsForValue();
+        valueOperations.set("hahahssss","yingyingying");
+
+//        System.out.println(valueOperations.get("laowang"));
     }
 
     @Test
     public void testRedis1(){
         ValueOperations valueOperations = redisTemplate.opsForValue();
-        Object lll = valueOperations.get("lll");
+        Object lll = valueOperations.get("laowang");
         System.out.println(lll);
     }
 }
